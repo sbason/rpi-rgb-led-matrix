acc02c00837bbd70a4afe5bc1370db6fe54d4dce
Sam Bason <sam.bason@gmail.com>
2017-12-10 18:25:36 +0000

    Make it pause every second

--- examples-api-use/test.cc
+++ examples-api-use/test.cc
@@ -32,120 +32,102 @@ static void InterruptHandler(int signo) {
   interrupt_received = true;
 }
 
-static void DrawOnCanvas(Canvas *canvas) {
-  /*
-   * Let's create a simple animation. We use the canvas to draw
-   * pixels. We wait between each step to have a slower animation.
-   */
-  // canvas->Fill(0, 0, 255);
-
-  while (!interrupt_received) {
-    int center_x = canvas->width() / 2;
-    int center_y = canvas->height() / 2;
-    float radius_max = canvas->width() / 2;
-    float angle_step = 1.0 / 360;
-    for (float a = 0; a < 255; a += 1) {
-      if (interrupt_received)
-        return;
-        canvas->Fill(a, 0, a);
-      // canvas->SetPixel(center_x, center_y,
-      //                  a, a, a);
-      usleep(1 * 10000);  // wait a little to slow down things.
-    }
-  }
-
-}
-
 static void WriteText(RGBMatrix * canvas) {
   FrameCanvas *offscreen = canvas->CreateFrameCanvas();
+  const char *bdf_font_file = "../fonts/4x6.bdf";
+  const char *bdf_big_font_file = "../fonts/clock.bdf";
+  const char *bdf_times_font_file = "../fonts/times.bdf";
+  Font font;
+  font.LoadFont(bdf_font_file);
+  Font bigFont;
+  bigFont.LoadFont(bdf_big_font_file);
+  Font timesFont;
+  timesFont.LoadFont(bdf_times_font_file);
+  struct timespec next_time;
+  next_time.tv_nsec = 0;
+  next_time.tv_sec = time(NULL);
+
   while (!interrupt_received) {
-    const char *bdf_font_file = "../fonts/4x6.bdf";
-    const char *bdf_big_font_file = "../fonts/clock.bdf";
-    const char *bdf_times_font_file = "../fonts/times.bdf";
-    Font font;
-    font.LoadFont(bdf_font_file);
-    Font bigFont;
-    bigFont.LoadFont(bdf_big_font_file);
-    Font timesFont;
-    timesFont.LoadFont(bdf_times_font_file);
+    try{
+      std::ifstream trainTimesFile("trainTimes.txt");
 
-    std::ifstream trainTimesFile("trainTimes.txt");
+      std::string red1, green1, blue1, time1, dest1;
+      std::getline(trainTimesFile, red1);
+      std::getline(trainTimesFile, green1);
+      std::getline(trainTimesFile, blue1);
+      std::getline(trainTimesFile, time1);
+      std::getline(trainTimesFile, dest1);
+      Color color1(std::stoi(red1), std::stoi(green1), std::stoi(blue1));
 
-    std::string red1, green1, blue1, time1, dest1;
-    std::getline(trainTimesFile, red1);
-    std::getline(trainTimesFile, green1);
-    std::getline(trainTimesFile, blue1);
-    std::getline(trainTimesFile, time1);
-    std::getline(trainTimesFile, dest1);
-    Color color1(std::stoi(red1), std::stoi(green1), std::stoi(blue1));
+      std::string red2, green2, blue2, time2, dest2;
+      std::getline(trainTimesFile, red2);
+      std::getline(trainTimesFile, green2);
+      std::getline(trainTimesFile, blue2);
+      std::getline(trainTimesFile, time2);
+      std::getline(trainTimesFile, dest2);
+      Color color2(std::stoi(red2), std::stoi(green2), std::stoi(blue2));
 
-    std::string red2, green2, blue2, time2, dest2;
-    std::getline(trainTimesFile, red2);
-    std::getline(trainTimesFile, green2);
-    std::getline(trainTimesFile, blue2);
-    std::getline(trainTimesFile, time2);
-    std::getline(trainTimesFile, dest2);
-    Color color2(std::stoi(red2), std::stoi(green2), std::stoi(blue2));
+      std::string red3, green3, blue3, time3, dest3;
+      std::getline(trainTimesFile, red3);
+      std::getline(trainTimesFile, green3);
+      std::getline(trainTimesFile, blue3);
+      std::getline(trainTimesFile, time3);
+      std::getline(trainTimesFile, dest3);
+      Color color3(std::stoi(red3), std::stoi(green3), std::stoi(blue3));
 
-    std::string red3, green3, blue3, time3, dest3;
-    std::getline(trainTimesFile, red3);
-    std::getline(trainTimesFile, green3);
-    std::getline(trainTimesFile, blue3);
-    std::getline(trainTimesFile, time3);
-    std::getline(trainTimesFile, dest3);
-    Color color3(std::stoi(red3), std::stoi(green3), std::stoi(blue3));
+      std::ifstream weatherFile("weather.txt");
+      std::string lowTemp, highTemp;
+      std::getline(weatherFile, lowTemp);
+      std::getline(weatherFile, highTemp);
 
-    std::ifstream weatherFile("weather.txt");
-    std::string lowTemp, highTemp;
-    std::getline(weatherFile, lowTemp);
-    std::getline(weatherFile, highTemp);
+      offscreen->Fill(0, 0, 0);
 
-    offscreen->Fill(0, 0, 0);
+      Color red(255, 0, 0);
+      Color yellow(255, 255, 0);
+      Color green(0, 255, 0);
+      Color white(255, 255, 255);
+      Color coldBlue(0, 228, 255);
+      char time_buffer[256];
+      char printTimeBuffer[256];
+      struct tm tm;
+      localtime_r(&next_time.tv_sec, &tm);
+      strftime(time_buffer, sizeof(time_buffer), "%H:%M", &tm);
+      strftime(printTimeBuffer, sizeof(time_buffer), "%H:%M:%S", &tm);
+      //1st destination
+      DrawText(offscreen, timesFont, 45, 5,
+        color1, NULL, time1.c_str(), 0);
+      DrawText(offscreen, font, 0, 5,
+        yellow, NULL, dest1.c_str(), 0);
 
-    Color red(255, 0, 0);
-    Color yellow(255, 255, 0);
-    Color green(0, 255, 0);
-    Color white(255, 255, 255);
-    Color coldBlue(0, 228, 255);
-    char time_buffer[256];
-    struct timespec next_time;
-    next_time.tv_sec = time(NULL);
-    next_time.tv_nsec = 0;
-    struct tm tm;
-    localtime_r(&next_time.tv_sec, &tm);
-    strftime(time_buffer, sizeof(time_buffer), "%H:%M", &tm);
-    //1st destination
-    DrawText(offscreen, timesFont, 45, 5,
-                         color1, NULL, time1.c_str(), 0);
-    DrawText(offscreen, font, 0, 5,
-                         yellow, NULL, dest1.c_str(), 0);
+      //2nd destination
+      DrawText(offscreen, timesFont, 45, 12,
+        color2, NULL, time2.c_str(), 0);
+      DrawText(offscreen, font, 0, 12,
+        yellow, NULL, dest2.c_str(), 0);
+      //3rd destination
+      DrawText(offscreen, timesFont, 45, 19,
+        color3, NULL, time3.c_str(), 0);
+      DrawText(offscreen, font, 0, 19,
+        yellow, NULL, dest3.c_str(), 0);
+      //clock
+      DrawText(offscreen, bigFont, 43, 32,
+        white, NULL, time_buffer, 0);
+      //temperatures
+      DrawText(offscreen, bigFont, 0, 32,
+        coldBlue, NULL, lowTemp.c_str(), 0);
+      DrawText(offscreen, bigFont, 15, 32,
+        red, NULL, highTemp.c_str(), 0);
 
-    //2nd destination
-    DrawText(offscreen, timesFont, 45, 12,
-                         color2, NULL, time2.c_str(), 0);
-    DrawText(offscreen, font, 0, 12,
-                         yellow, NULL, dest2.c_str(), 0);
-    //3rd destination
-    DrawText(offscreen, timesFont, 45, 19,
-                         color3, NULL, time3.c_str(), 0);
-    DrawText(offscreen, font, 0, 19,
-                         yellow, NULL, dest3.c_str(), 0);
-    //clock
-    DrawText(offscreen, bigFont, 43, 32,
-                         white, NULL, time_buffer, 0);
-    //temperatures
-    DrawText(offscreen, bigFont, 0, 32,
-                         coldBlue, NULL, lowTemp.c_str(), 0);
-    DrawText(offscreen, bigFont, 15, 32,
-                         red, NULL, highTemp.c_str(), 0);
-    // DrawText(offscreen, bigFont, 0, 31,
-    //                      white, NULL, "1234567890", -1);
+      clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &next_time, NULL);
 
-     // Atomic swap with double buffer
-     offscreen = canvas->SwapOnVSync(offscreen);
+      // Atomic swap with double buffer
+      offscreen = canvas->SwapOnVSync(offscreen);
 
-     next_time.tv_sec += 1;
-    // canvas->Clear();
+
+    } catch(const std::exception& e) {
+      printf("An error occured");
+    }
+    next_time.tv_sec += 1;
   }
 }
 
