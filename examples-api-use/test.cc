48a680143d6e44d5cc9a1405d81d175598cf2279
Sam Bason <sam.bason@gmail.com>
2017-12-28 20:32:22 +0000

    Do cloudy and partly sunny

--- examples-api-use/test.cc
+++ examples-api-use/test.cc
@@ -73,6 +73,7 @@ static void MakeCloudySymbol(FrameCanvas *offscreen) {
   //line 8
   DrawWeatherLine(offscreen, 5, 8, 9, 8, lightGrey);
 }
+
 static void MakeSunnySymbol(FrameCanvas *offscreen) {
   Color orange(240, 198, 0);
 
@@ -97,6 +98,70 @@ static void MakeSunnySymbol(FrameCanvas *offscreen) {
   //line 8
   DrawWeatherLine(offscreen, 5, 8, 5, 8, orange);
   DrawWeatherLine(offscreen, 8, 8, 8, 8, orange);
+}
+
+static void MakePartlyCloudySymbol(FrameCanvas *offscreen) {
+  Color orange(240, 198, 0);
+  Color white(255, 255, 255);
+  Color lightGrey(232, 232, 230);
+  //line 1
+  DrawWeatherLine(offscreen, 5, 1, 5, 1, orange);
+  DrawWeatherLine(offscreen, 8, 1, 8, 1, orange);
+  //line 2 blank
+  //line 3
+  DrawWeatherLine(offscreen, 3, 3, 3, 3, orange);
+  DrawWeatherLine(offscreen, 5, 3, 8, 3, orange);
+  DrawWeatherLine(offscreen, 10, 3, 10, 3, orange);
+  //line 4
+  DrawWeatherLine(offscreen, 5, 4, 7, 4, orange);
+  DrawWeatherLine(offscreen, 8, 4, 9, 4, lightGrey);
+  //line 5
+  DrawWeatherLine(offscreen, 4, 5, 5, 5, lightGrey);
+  DrawWeatherLine(offscreen, 6, 5, 6, 5, orange);
+  DrawWeatherLine(offscreen, 7, 5, 7, 5, lightGrey);
+  DrawWeatherLine(offscreen, 8, 5, 9, 5, white);
+  DrawWeatherLine(offscreen, 10, 5, 10, 5, lightGrey);
+  //line 6
+  DrawWeatherLine(offscreen, 3, 6, 3, 6, lightGrey);
+  DrawWeatherLine(offscreen, 4, 6, 5, 6, white);
+  DrawWeatherLine(offscreen, 6, 6, 6, 6, orange);
+  DrawWeatherLine(offscreen, 7, 6, 9, 6, white);
+  DrawWeatherLine(offscreen, 10, 6, 10, 6, lightGrey);
+  //line 7
+  DrawWeatherLine(offscreen, 3, 7, 3, 7, lightGrey);
+  DrawWeatherLine(offscreen, 4, 7, 9, 7, white);
+  DrawWeatherLine(offscreen, 10, 7, 10, 7, lightGrey);
+  //line 8
+  DrawWeatherLine(offscreen, 4, 8, 9, 8, lightGrey);
+}
+
+static void MakeLightRainSymbol(FrameCanvas *offscreen) {
+  Color blue(98, 184, 244);
+  Color white(255, 255, 255);
+  Color lightGrey(232, 232, 230);
+  //line 1
+  DrawWeatherLine(offscreen, 5, 1, 9, 1, lightGrey);
+  //line 2 blank
+  DrawWeatherLine(offscreen, 4, 2, 4, 2, lightGrey);
+  DrawWeatherLine(offscreen, 5, 2, 9, 2, white);
+  DrawWeatherLine(offscreen, 10, 2, 10, 2, lightGrey);
+  //line 3
+  DrawWeatherLine(offscreen, 3, 3, 3, 3, lightGrey);
+  DrawWeatherLine(offscreen, 4, 3, 10, 3, white);
+  DrawWeatherLine(offscreen, 11, 3, 11, 3, lightGrey);
+  //line 4
+  DrawWeatherLine(offscreen, 3, 4, 3, 4, lightGrey);
+  DrawWeatherLine(offscreen, 4, 4, 9, 4, white);
+  DrawWeatherLine(offscreen, 10, 4, 10, 4, lightGrey);
+  //line 5
+  DrawWeatherLine(offscreen, 4, 5, 9, 5, lightGrey);
+  //line 6
+  DrawWeatherLine(offscreen, 5, 6, 5, 6, blue);
+  DrawWeatherLine(offscreen, 8, 6, 8, 6, blue);
+  //line 7 blank
+  //line 8
+  DrawWeatherLine(offscreen, 6, 8, 6, 8, blue);
+  DrawWeatherLine(offscreen, 9, 8, 9, 8, blue);
 }
 
 static void WriteText(RGBMatrix * canvas) {
@@ -185,7 +250,7 @@ static void WriteText(RGBMatrix * canvas) {
         red, NULL, highTemp.c_str(), 0);
 
       //weather
-      MakeSunnySymbol(offscreen);
+      MakeLightRainSymbol(offscreen);
 
       clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &next_time, NULL);
 
